b49e3a9636aa2bb1f0048164a900e7adb0acb474
yqiu <qiuyj@lansee.net>
2017-11-27 15:47:21 +0800

    ysl

--- src/page/topic/ysl/index.vue
+++ src/page/topic/ysl/index.vue
@@ -2,13 +2,15 @@
     <router-view></router-view>
 </template>
 <script>
-    import common from "../../../common/common";
-
-    let appId = 'wxa408e026b5511183',
-        redirectURI = 'http://mm.qioacat.com/topic-ysl',
-        wxLogin = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + appId + '&redirect_uri=' + redirectURI + '&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect';
-    let code = common.getQueryString("code") || '';
-    console.log(code);
-    if (!code)
-        location.href = wxLogin;
+    export default {
+        created () {
+            let appId = 'wxa408e026b5511183',
+                redirectURI = 'http://mm.qioacat.com/topic-ysl',
+                wxLogin = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + appId + '&redirect_uri=' + redirectURI + '&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect';
+            let code = this.$route.query.code || '';
+            console.log(code);
+            if (!code)
+                location.href = wxLogin;
+        }
+    }
 </script>
