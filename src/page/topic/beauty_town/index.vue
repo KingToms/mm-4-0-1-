2b9154fcac9aa8a998780a34d6251fe4e1817a31
yqiu <qiuyj@lansee.net>
2018-01-18 09:48:11 +0800

    预加载分享图片

--- src/page/topic/beauty_town/index.vue
+++ src/page/topic/beauty_town/index.vue
@@ -155,7 +155,8 @@
                             let imgS = [
                                 '/static/topic/beauty_down/index/web/box_head.png',
                                 '/static/topic/beauty_down/index/web/box_end.png',
-                                '/static/topic/beauty_down/index/web/1px.png'
+                                '/static/topic/beauty_down/index/web/1px.png',
+                                'http://mm.qiaocat.com/static/topic/beauty_down/luckdraw_3/share_1.jpg'
                             ];
                             for (let i = 1; i <= 8; i++) {
                                 imgS.push(`/static/topic/beauty_down/index/brand/10/img${i}.jpg`)
